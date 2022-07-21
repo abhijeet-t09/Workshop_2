8fd18bb088a947fd0fb29d9499a3d8a62b46cd37
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-07-21 14:36:29 +0530

    Person Details: Second Commit

---
+++ src/com/bridgelabz/Contact.java
@@ -0,0 +1,58 @@
+package com.bridgelabz;
+
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String city;
+    private  String state;
+    private String zipCode;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(String firstName, String lastName, String city, String state, String pinCode, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.zipCode = pinCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public Contact() {
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setPinCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+}
